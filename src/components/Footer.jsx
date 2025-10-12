664bf508f148464f9bb576d342816266be967955
Dzarooo <155550980+Dzarooo@users.noreply.github.com>
2025-10-12 23:31:33 +0200

    fix to footer overflowing with corners on phones

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <div className="min-h-fit max-h-[100px] h-[3vh] lg:h-[5vh] bg-yellow-400 flex items-center px-2 z-1 [box-shadow:0px_0px_6px_-1px_rgb(0_0_0_/_0.3)]">
-            <p className="text-xs lg:text-sm">Lody Misia Wojtka. Wszystkie prawa zastrzeżone.</p>
+            <p className="text-xs lg:text-sm px-3 lg:!px-5">Lody Misia Wojtka. Wszystkie prawa zastrzeżone.</p>
         </div>
     )
 }
